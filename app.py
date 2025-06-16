97c74dda937aff1c25a8a756a4b8df5f0bdd0966
polarbroski <166693319+polarbroski@users.noreply.github.com>
2025-06-16 15:25:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -32,5 +32,8 @@ def index():
 
     return render_template("index.html")
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render가 자동으로 포트 지정
+    app.run(host='0.0.0.0', port=port, debug=True)
