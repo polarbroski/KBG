876fad905fd8e66323be31249d66045ce860c4fb
polarbroski <166693319+polarbroski@users.noreply.github.com>
2025-06-16 16:29:12 +0900

    Update rag_llm.py

--- rag_llm.py
+++ rag_llm.py
@@ -12,7 +12,7 @@ def generate_summary_from_docs(player_name, documents):
     )
 
     llm = ChatAnthropic(
-        model="claude-3-sonnet-20240229",  # 또는 최신 버전 사용 가능
+        model="claude-sonnet-4-20250514",  # 또는 최신 버전 사용 가능
         temperature=0.3,
         max_tokens=1024
     )
